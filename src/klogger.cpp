42424099d52f7c07bddd6905be35283b9553d089
KatzaShork <114446618+KatzaCat@users.noreply.github.com>
2026-04-17 00:46:42 -0500

    fixed logs not printing new line chars

--- src/klogger.cpp
+++ src/klogger.cpp
@@ -1,5 +1,8 @@
 #include "klogger.hpp"
 
+#include <cstdio>
+#include <cstdlib>
+
 // what will be used as the "pull" functions
 static k::PullResult _current_log = {};
 
@@ -33,18 +36,24 @@ static const std::string logSeverityToString(k::LogSeverity log_severity) {
 // Basic logging methods
 
 void k::Logger::info(const std::string message) {
+  std::printf("%-8s %s\n", logSeverityToString(k::INFO).c_str(), message.c_str());
 }
 
 void k::Logger::debug(const std::string message) {
+  std::printf("%-8s %s\n", logSeverityToString(k::DEBUG).c_str(), message.c_str());
 }
 
 void k::Logger::warning(const std::string message) {
+  std::printf("%-8s %s\n", logSeverityToString(k::WARNING).c_str(), message.c_str());
 }
 
 void k::Logger::error(const std::string message) {
+  std::printf("%-8s %s\n", logSeverityToString(k::ERROR).c_str(), message.c_str());
 }
 
 void k::Logger::critical(const std::string message) {
+  std::printf("%-8s %s\n", logSeverityToString(k::CRITICAL).c_str(), message.c_str());
+  exit(-1);
 }
 
 // Push based logging
@@ -58,4 +67,24 @@ k::PullResult k::Logger::pullCurrentLog()
 {return _current_log;}
 
 void k::Logger::printCurrentLog() {
+  switch (_current_log.log_severity) {
+    case k::INFO:
+      k::Logger::info(_current_log.message);
+      break;
+    case k::DEBUG:
+      k::Logger::debug(_current_log.message);
+      break;
+    case k::WARNING:
+      k::Logger::warning(_current_log.message);
+      break;
+    case k::ERROR:
+      k::Logger::error(_current_log.message);
+      break;
+    case k::CRITICAL:
+      k::Logger::critical(_current_log.message);
+      break;
+
+    default: break;
+
+  }
 }
