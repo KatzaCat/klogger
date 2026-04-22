6f269b0ec5672bb95a9962fbe2fcba5a0e0438a9
KatzaShork <114446618+KatzaCat@users.noreply.github.com>
2026-04-21 22:03:08 -0500

    reformatting

--- src/klogger.cpp
+++ src/klogger.cpp
@@ -22,7 +22,7 @@
  */
 
 // what will be used as the "pull" functions
-static k::LogInformation _current_log = {};
+static k::LogInformation _current_log_information = {};
 
 // Stores the color and text sepaeately
 struct SeverityResult {
@@ -31,12 +31,12 @@ struct SeverityResult {
 };
 
 void k::Logger::setOutputDestination(k::OutputDestination destination) 
-{_current_log.destination = destination;}
+{_current_log_information.destination = destination;}
 
 // Dealing with logging to a file
 
 void k::Logger::setOutputFile(const std::string file) {
-  _current_log.file_destination = file;
+  _current_log_information.file_destination = file;
   if (std::filesystem::exists(file)) {
     // clears the file;
     std::ofstream clear_file(file, std::ios::trunc);
@@ -45,7 +45,7 @@ void k::Logger::setOutputFile(const std::string file) {
 }
 
 // returns the log severity as a string, makes it so i dont have to type this all out manualy :3
-static SeverityResult logSeverityResult(k::LogSeverity log_severity) {
+static SeverityResult _logSeverityResult(k::LogSeverity log_severity) {
   SeverityResult log_severity_result = {};
 
   switch (log_severity) {
@@ -78,7 +78,7 @@ static SeverityResult logSeverityResult(k::LogSeverity log_severity) {
 
 // Basic logging methods
 
-static const std::string getTimeAsString() {
+static const std::string _getTimeAsString() {
   std::time_t current_time = std::time(NULL);
   // localtime is depricated, but ill look for a 
   // alternitive later
@@ -90,38 +90,38 @@ static const std::string getTimeAsString() {
   return buffer;
 }
 
-static void logToStdOut(k::LogSeverity log_severity, const std::string message) {
-  SeverityResult severity_result = logSeverityResult(log_severity);
+static void _logToStdOut(k::LogSeverity log_severity, const std::string message) {
+  SeverityResult severity_result = _logSeverityResult(log_severity);
    
   std::println(
       "[{}] {}{:>9}{} {}", 
-      getTimeAsString(),
+      _getTimeAsString(),
       severity_result.color, severity_result.text, k::col::reset(), 
       message);
 }
 
-static void logToFile(k::LogSeverity log_severity, const std::string message) {
-  SeverityResult severity_result = logSeverityResult(log_severity);
+static void _logToFile(k::LogSeverity log_severity, const std::string message) {
+  SeverityResult severity_result = _logSeverityResult(log_severity);
    
-  std::ofstream file(_current_log.file_destination, std::ios::app);
+  std::ofstream file(_current_log_information.file_destination, std::ios::app);
 
   if (!file.is_open()) {
-    std::println("Failed to open \"{}\"", _current_log.file_destination);
+    std::println("Failed to open \"{}\"", _current_log_information.file_destination);
     return;
   }
 
-  file << std::format("[{}] {:>9} {}\n", getTimeAsString(), severity_result.text, message);
+  file << std::format("[{}] {:>9} {}\n", _getTimeAsString(), severity_result.text, message);
 
   file.close();
 }
 
-static void log(k::LogSeverity log_severity, const std::string message) {
-  switch (_current_log.destination) {
+static void _log(k::LogSeverity log_severity, const std::string message) {
+  switch (_current_log_information.destination) {
     case k::STD_OUT:
-      logToStdOut(log_severity, message);
+      _logToStdOut(log_severity, message);
       break;
     case k::FILE:
-      logToFile(log_severity, message); 
+      _logToFile(log_severity, message); 
       break;
 
     default: break;
@@ -129,53 +129,53 @@ static void log(k::LogSeverity log_severity, const std::string message) {
 }
 
 void k::Logger::info(const std::string message) 
-{log(k::INFO, message);}
+{_log(k::INFO, message);}
 
 void k::Logger::debug(const std::string message) 
-{log(k::DEBUG, message);}
+{_log(k::DEBUG, message);}
 
 void k::Logger::warning(const std::string message) 
-{log(k::WARNING, message);}
+{_log(k::WARNING, message);}
 
 void k::Logger::error(const std::string message) 
-{log(k::ERROR, message);}
+{_log(k::ERROR, message);}
 
 void k::Logger::critical(const std::string message) {
-  log(k::CRITICAL, message);
+  _log(k::CRITICAL, message);
   exit(-1);
 }
 
 // Push based logging
 
 void k::Logger::pushNewLog(const std::string message, k::LogSeverity log_severity) {
-  _current_log.message = message; 
+  _current_log_information.message = message; 
   
-  _current_log.log_severity = log_severity;
+  _current_log_information.log_severity = log_severity;
 
-  SeverityResult severity_result = logSeverityResult(log_severity);
-  _current_log.log_text = severity_result.text;
-  _current_log.log_color = severity_result.color;
+  SeverityResult severity_result = _logSeverityResult(log_severity);
+  _current_log_information.log_text = severity_result.text;
+  _current_log_information.log_color = severity_result.color;
 }
 
 k::LogInformation k::Logger::pullCurrentLog() 
-{return _current_log;}
+{return _current_log_information;}
 
 void k::Logger::printCurrentLog() {
-  switch (_current_log.log_severity) {
+  switch (_current_log_information.log_severity) {
     case k::INFO:
-      k::Logger::info(_current_log.message);
+      k::Logger::info(_current_log_information.message);
       break;
     case k::DEBUG:
-      k::Logger::debug(_current_log.message);
+      k::Logger::debug(_current_log_information.message);
       break;
     case k::WARNING:
-      k::Logger::warning(_current_log.message);
+      k::Logger::warning(_current_log_information.message);
       break;
     case k::ERROR:
-      k::Logger::error(_current_log.message);
+      k::Logger::error(_current_log_information.message);
       break;
     case k::CRITICAL:
-      k::Logger::critical(_current_log.message);
+      k::Logger::critical(_current_log_information.message);
       break;
 
     default: break;
