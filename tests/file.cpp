6c3cc6d44a3f2e2249c0b8dded65a9079e6d7a1b
KatzaShork <114446618+KatzaCat@users.noreply.github.com>
2026-04-21 21:32:15 -0500

    added a test for logging into a file

---
+++ tests/file.cpp
@@ -0,0 +1,14 @@
+#include <klogger.hpp>
+
+int main() {
+  k::Logger::setOutputDestination(k::FILE);
+  k::Logger::setFileOutput("out.txt");
+
+  k::Logger::info("file test");
+  k::Logger::error("something went wrong(not really) uwu");
+
+  k::Logger::pushNewLog("pushing a log", k::WARNING);
+  k::Logger::printCurrentLog();
+
+  return 0;
+}
